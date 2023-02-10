c5aa38d2ea5bd4d580497c2ace3faf1de2971372
john <izolotavin99@gmail.com>
2023-02-10 12:25:37 +0300

    1.Init commit 2.users generated

---
+++ src/database.config.ts
@@ -0,0 +1,21 @@
+import { DataSource } from 'typeorm';
+
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: process.env.POSTGRES_HOST || 'localhost',
+  port: Number(process.env.POSTGRES_PORT) || 5432,
+  username: process.env.POSTGRES_USER || 'postgres',
+  password: process.env.POSTGRES_PASSWORD || 'postgres',
+  database: process.env.POSTGRES_DB || 'library_db',
+  entities: [__dirname + '/**/*.entity{.ts,.js}'],
+  synchronize: false,
+  migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
+});
+
+// AppDataSource.initialize()
+//   .then(() => {
+//     console.log('Data Source has been initialized!');
+//   })
+//   .catch((err) => {
+//     console.error('Error during Data Source initialization', err);
+//   });
