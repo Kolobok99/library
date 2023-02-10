3f29826d7e1c972dad5eb9c212500b706c8935ae
john <izolotavin99@gmail.com>
2023-02-10 13:22:09 +0300

    1. users e.p. added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppDataSource } from './database.config';
 
 @Module({
-  imports: [UsersModule],
+  imports: [TypeOrmModule.forRoot(AppDataSource.options), UsersModule],
   providers: [],
   controllers: [],
 })
